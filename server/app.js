65e111e70d29e77d64c089a136485ded5e4c10cb
Brian Lee <brianjleeofcl@gmail.com>
2017-02-24 11:16:08 -0800

    Add dotenv

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const path = require('path');
