978f799cc917dc8cb6c44fdb6f30c20d768c3079
Hiromi Heider <hiromiheider@gmail.com>
2017-02-24 10:32:01 -0800

    server stuff

--- server/app.js
+++ server/app.js
@@ -1,10 +1,54 @@
+'use strict';
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
+
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const app = express();
+
+//what is this?
+if (process.env.NODE_ENV !== 'test') {
+  const logger = require('morgan')
+  app.use(logger('dev'))
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, '/../', 'public')));
+app.use(express.static(path.join(__dirname, '/../', 'node_modules')));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.use(morgan('dev'));
 
-app.use('/api', require('./routes/api'))
+app.use('/api', require('./routes/api'));
+
+// app.get('*', (req, res) => {
+//   console.log('this is a test');
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
+
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, '/../', 'public')})
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  console.log(err)
+  res.status(err.status || 500)
+  res.json(err)
+});
 
 module.exports = app;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   res.send('Weeeeeeeee')
 })
 
