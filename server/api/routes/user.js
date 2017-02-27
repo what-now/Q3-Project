2aee4eb3cef244214e6a0dbac6ff5e0c9feb54f2
Hiromi Heider <hiromiheider@gmail.com>
2017-02-27 14:55:15 -0800

    handling newUser post error/added cookie

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const knex = require('../../knex');
 const bcrypt = require('bcrypt');
 const boom = require('boom');
+const jwt = require('jsonwebtoken');
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const { email, password, name } = req.body
 
   knex('users').where('email', email).then(arr => {
@@ -19,8 +20,20 @@ router.post('/', (req, res) => {
     const registeredUser = arr[0]
     delete registeredUser.h_pw
 
+    const claim = { id: registeredUser.id };
+    const token = jwt.sign(claim, process.env.JWT_KEY, {
+      expiresIn: '7 days'
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      expiresIn: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      secure: router.get('env') === 'production'
+    })
+
     res.send(registeredUser)
   })
+  .catch((err) => next(err))
 })
 
 module.exports = router;
