72c5cdafa0d192a52d98a1e83192b3c436d3a16a
Hiromi Heider <hiromiheider@gmail.com>
2017-02-27 09:55:47 -0800

    angular form validation

--- auth/public/js/login/login.component.js
+++ auth/public/js/login/login.component.js
@@ -4,6 +4,7 @@
   class controller {
     constructor(loginService) {
       this.loginService = loginService
+      this.regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     }
 
     login(event) {
